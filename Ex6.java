e60956dfdf209ce10a21300f45aab3cad2b8a536
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-02-21 13:18:02 +0000

    Lista 1A - Exercício 6

--- Ex6.java
+++ Ex6.java
@@ -1,1 +1,22 @@
 import java.util.Scanner;
+
+public class Ex6{
+    public static  Double mulComAdicao(Double a, int b){
+        Double resultado = 0.00;
+        for (Double i = 0.00; i < b; i++){
+            resultado += a;
+            // System.out.println("Calculando:" + resultado);
+        }
+        return resultado;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Digite um número: ");
+        Double num1 = scanner.nextDouble();
+        System.out.print("Digite outro número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.println("O resultado da multicação desses dois números é: " + mulComAdicao(num1, num2));
+        scanner.close();
+    }
+}
