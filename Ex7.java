7740fbbd3b38d66913eee503b89a3862ef296725
Carolina Lee <111212023+roxo-1@users.noreply.github.com>
2025-02-21 13:48:52 +0000

    Lista 1A - Exercício 7

--- Ex7.java
+++ Ex7.java
@@ -1,7 +1,24 @@
 import java.util.Scanner;
 
 public class Ex7{
+    public static String contarVogais(String str){
+        int numeroDeCaracteres = str.length();
+        int qtdvogais=0;
+        char[] charArray = str.toCharArray();
+        for (int i = 0; i < numeroDeCaracteres; i++){
+            char c = Character.toUpperCase(charArray[i]);
+            if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+              qtdvogais ++;
+            }
+            
+        }
+        return "O número de vogais nesta frase: " + qtdvogais;
+    }
     public static void main(String[] args){
-        //
+        Scanner scanner = new Scanner (System.in);
+        System.out.print("Digite uma frase: ");
+        String str = scanner.nextLine();
+        System.out.println(contarVogais(str));
+        scanner.close();
     }
 }
